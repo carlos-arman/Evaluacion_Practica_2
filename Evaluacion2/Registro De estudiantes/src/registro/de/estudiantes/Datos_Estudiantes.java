27c4772c045e2aca376873a114d288006fad98cb
melvinantonio <melvinantonioangellopez@gmail.com>
2022-05-04 11:55:00 -0600

    Aporte a la clase

---
+++ Evaluacion2/Registro De estudiantes/src/registro/de/estudiantes/Datos_Estudiantes.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package registro.de.estudiantes;
+
+/**
+ *
+ * @author Armando
+ */
+public class Datos_Estudiantes {
+
+    /**
+     * @param args the command line arguments
+     */
+     private int id_estudiante;
+    private int carnet_estudiante;
+    private String nom_estudiante;
+    private String ape_estudiante;
+    private int edad_estudiante;
+
+    public int getId_estudiante() {
+        return id_estudiante;
+    }
+
+    public void setId_estudiante(int id_estudiante) {
+        this.id_estudiante = id_estudiante;
+    }
+
+    public int getCarnet_estudiante() {
+        return carnet_estudiante;
+    }
+
+    public void setCarnet_estudiante(int carnet_estudiante) {
+        this.carnet_estudiante = carnet_estudiante;
+    }
+
+    public String getNom_estudiante() {
+        return nom_estudiante;
+    }
+
+    public void setNom_estudiante(String nom_estudiante) {
+        this.nom_estudiante = nom_estudiante;
+    }
+
+    public String getApe_estudiante() {
+        return ape_estudiante;
+    }
+
+    public void setApe_estudiante(String ape_estudiante) {
+        this.ape_estudiante = ape_estudiante;
+    }
+
+    public int getEdad_estudiante() {
+        return edad_estudiante;
+    }
+
+    public void setEdad_estudiante(int edad_estudiante) {
+        this.edad_estudiante = edad_estudiante;
+    }
+    
+}
