cbe032ba35a66883359e698402b64496755a91b2
carlos-arman <minerocarlos28@gmail.com>
2022-05-04 11:56:13 -0700

    CLASE CONEXION Y METODOS

--- Evaluacion2/Registro De estudiantes/src/Conexion/Cls_conexion.java
+++ Evaluacion2/Registro De estudiantes/src/Conexion/Cls_conexion.java
@@ -2,12 +2,25 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+
 package Conexion;
+import java.sql.*;
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author Armando
  */
-public class Cls_conexion {
+public class Cls_conexion{ 
     
+    Connection conect = null;
+    public Connection conexion(){
+        try{
+            Class.forName("org.gjt.mm.mysql.Driver");
+            conect = DriverManager.getConnection("jdbc:mysql://localhost/bd","root ", "");
+        }catch (Exception e){
+            JOptionPane.showMessageDialog(null, "Error" + e);
+        }
+     return conect;
+    }
 }
