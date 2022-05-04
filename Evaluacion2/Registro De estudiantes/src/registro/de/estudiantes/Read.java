b28ba9a48e3724cb82878fe5a22ade5394199a9d
Kevin Torres <101214932+superkjr@users.noreply.github.com>
2022-05-04 13:39:18 -0600

    clase read

---
+++ Evaluacion2/Registro De estudiantes/src/registro/de/estudiantes/Read.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package registro.de.estudiantes;
+
+import Conexion.Cls_conexion;
+import java.sql.SQLException;
+
+
+public class Read {
+     public Read () throws SQLException {
+        System.out.println("<< CONSULTA DE REGISTROS >>");
+        Resultado();
+    }
+    private void Resultado () throws SQLException {
+        try {
+            Cls_conexion mostrar = new Cls_conexion ();
+            
+                String tabla = "tb_estudiante";
+                String camposTabla = "*";
+                String condicionBusqueda = "";
+                
+                mostrar.desplegarRegistros(tabla, camposTabla, condicionBusqueda);
+        }catch (SQLException ex){
+            System.out.println("Ha ocurrido el siguiente error: " + ex.getMessage());
+        }finally{
+            CLASE_PRINCIPAL.MenuDesplegable();
+        }
+    }
+}
